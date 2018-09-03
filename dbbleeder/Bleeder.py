55c5a179e001681184fe6eb760e3f656f676079e
Todd Roper <toddroper@me.com>
2018-09-03 17:22:34 -0600

    Implemented threading.

--- dbbleeder/Bleeder.py
+++ dbbleeder/Bleeder.py
@@ -1,4 +1,4 @@
-import thread
+from threading import Thread, ThreadError
 from dbbleeder.datatools.BleedDB import BleedDB
 from dbbleeder.datatools.Table import Table
 
@@ -18,16 +18,21 @@ class Bleeder:
     def boot(self):
         # If we aren't doing a DB operation, set the table name.
         if self.mode != "db":
-            self.name = self.config["source"]["table"]["name"]
+            self.name = self.config["source"]["tables"]["name"]
         # Now run delegate ops for copying the db/table/data.
         if self.mode == "db":
             self.copy_tables()
         elif self.mode == "table":
             self.copy_table()
         else:
+            self.get_table()
             self.copy_data()
 
+        # Turn foreign key checks back on
+        self.destination.toggle_fk()
+
     def copy_tables(self):
+        self.source.get_tables()
         for table in self.source.tables:
             self.name = table
             if self.create is not False:
@@ -40,11 +45,24 @@ class Bleeder:
 
     def copy_table(self):
         self.get_table(self.create)
+        rows = self.current_table.get_row_count()
+        batch_size = self.current_table.batch_size
+        count = 0
+        threads = 0
+        bleeder = self
 
-    def copy_data(self):
-        if self.current_table is False:
-            self.get_table()
-        self.current_table.copy_data()
+        while rows > 0:
+            if threads < 4:
+                name = "Thread" + str(threads)
+                Thread(target=bleeder.copy_data(count), name=name)
+                count += batch_size
+                rows -= batch_size
+            print "Count:"
+            print count
+            print rows
+
+    def copy_data(self, start=0):
+        print self.current_table.copy_data(start)
 
     def create_table(self):
         if isinstance(self.name, str) or isinstance(self.name, unicode):
@@ -57,8 +75,7 @@ class Bleeder:
         self.get_table(True)
 
     def get_table(self, build=False):
-        self.current_table = Table(self.name, self.source, self.destination, self.create, self.replace)
+        self.current_table = Table(self.name, self.mode, self.source, self.destination, self.create, self.replace)
 
         if build is True:
             self.current_table.build_table()
-
